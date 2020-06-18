399e6b5c1f5400597c33845c0361a8327ce85029
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-18 00:32:01 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :authors, only: [:new, :show, :edit, :update, :create]
+  resources :posts, only: [:new, :show, :edit, :update, :create]
+
 end
